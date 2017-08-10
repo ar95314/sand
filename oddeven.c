52118fa07fb7662d47bac3b734572f00817216f2
dhoni007 <30770814+dhoni007@users.noreply.github.com>
2017-08-10 18:39:09 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+int n;
+printf("enter n");
+scanf("%d",&n);
+if(n%2==0)
+ {
+  printf("\n it is even");
+ }
+ else
+ {
+ printf("\n it is odd");
+ }
+	return 0;
+}
