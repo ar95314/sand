affaa5624c233e44be0790d736d5ef27f534143a
dhoni007 <30770814+dhoni007@users.noreply.github.com>
2017-08-11 19:51:22 +0530

    Update sumN.c

--- sumN.c
+++ sumN.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main(void)
-{
- int N,i;
- int sum=0;
- printf("Enter the real number");
- scanf("%d",&N);
- for(i=1;i<=N;i++)
- {
- 	sum=sum+i;
- }
- printf("\n sum is %d",sum);
+int main(void) {
+	int i,N;
+	int sum=0;
+	printf("Enter N");
+	scanf("%d",&N);
+	for(i=1;i<=N;i++)
+	{
+		sum=sum+i;
+	}
+	printf("\n sum of N is %d",sum);
 	return 0;
 }
+
