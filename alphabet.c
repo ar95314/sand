156f4488d96bf795c50c355c9469eab73287667e
Sandhiya M <30770814+dhoni007@users.noreply.github.com>
2018-06-28 10:21:17 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-int main(void) 
-{
-	char c;
+int main()
+	{char c;
 	scanf("%c",&c);
-	printf("Enter the character");
-	if(c=='a'||c=='b'||c=='c'||c=='d'||c=='e'||c=='f'||c=='g'||c=='h'||c=='i'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='o'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='u'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z'||c=='A'||c=='B'||c=='C'||c=='D'||c=='E'||c=='F'||c=='G'||c=='H'||c=='I'||c=='J'||c=='K'||c=='L'||c=='M'||c=='N'||c=='O'||c=='P'||c=='Q'||c=='R'||c=='S'||c=='T'||c=='U'||c=='V'||c=='W'||c=='X'||c=='Y'||c=='Z')
-	{
-		printf("\n c is an alphabet");
-	}
+	if(c >='a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	printf("yes");
 	else
-	{
-	printf("\n c is not an alphabet");
-	}
-	return 0;
+	printf("no");
+     return 0;
 }
