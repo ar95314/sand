f49b92a55f1088a24a44dc5f061ee885eef3ff84
Sandhiya M <30770814+dhoni007@users.noreply.github.com>
2018-07-01 05:29:23 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num , rem = 0 , temp , var ;
+	scanf ("%d", &num) ;
+	temp = num;
+	while (num != 0)
+	{
+		var = num % 10 ;
+		rem = rem * 10 + var ;
+		num = num / 10 ;
+	}
+	if ( temp == rem)
+	   printf("palindrome") ;
+	else
+	   printf("not palindrome") ;
+	return 0;
+}
